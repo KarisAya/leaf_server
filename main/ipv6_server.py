59e3f2dcaecf4196a016f3a81d31256d5f3ce0b1
KarisAya <1048827424@qq.com>
2023-11-14 19:03:12 +0800

    commit

--- main/ipv6_server.py
+++ main/ipv6_server.py
@@ -4,6 +4,7 @@ import http.client
 import os
 import sys
 import random
+import json
 from pathlib import Path
 from redirect_ipv6 import get_my_IPv6
 from Server import Server
@@ -11,6 +12,13 @@ from Server import Server
 
 class CustomHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
+        self.path = self.path.rstrip("/") or "/"
+        if self.path in server.WEB_DICT:
+            return server.WEB_DICT[self.path](self)
+        self.send_response(404)
+        self.end_headers()
+
+    def do_POST(self):
         self.path = self.path.rstrip("/") or "/"
         if self.path in server.WEB_DICT:
             return server.WEB_DICT[self.path](self)
@@ -34,6 +42,23 @@ if __name__ == "__main__":
     @server.api("/image", image_list=server.list_subpaths("/image/"))
     def _(handler: CustomHandler, image_list):
         server.WEB_DICT[random.choice(image_list)](handler)
+
+    @server.api("/terminal")
+    def _(handler: CustomHandler):
+        content_length = int(handler.headers["Content-Length"])
+        post_data = handler.rfile.read(content_length)
+        data = json.loads(post_data).get("data") if post_data else "none"
+        handler.send_response(200)
+        handler.end_headers()
+        resp = {}
+        # 假的响应
+        if data == "init":
+            resp["path"] = "/server"
+            resp["contect"] = ""
+        else:
+            resp["path"] = "/server"
+            resp["contect"] = f"Recived:{data}"
+        handler.wfile.write(json.dumps(resp).encode())
 
     server.path_mapping("/list", "/")
     server.listing_src("/list", src)  # 用目录覆盖映射
--- page/server/src/index.ts
+++ page/server/src/index.ts
@@ -1,12 +1,40 @@
 import "./style/index.less"
 
-const ImagePath = window.location.origin + '/image';
-document.getElementById('background')!.style.backgroundImage = `url(${ImagePath})`;
+document.getElementById('background')!.style.backgroundImage = `url(${'/image'})`;
 
-const InfoPath = window.location.origin + '/info';
-const info = document.getElementById("info") as HTMLObjectElement;
-info.data = InfoPath;
-
-const ListPath = window.location.origin + '/list';
 const list = document.getElementById("list") as HTMLObjectElement;
-list.data = ListPath;
+list.data = '/list';
+
+const output = document.getElementById("output") as HTMLDivElement;
+const input_text = document.getElementById("input_text") as HTMLInputElement;
+const input_button = document.getElementById("input_button") as HTMLButtonElement;
+
+input_button.addEventListener('click', () => { sendData(input_text.value) });
+input_text.addEventListener('keydown', (event) => { if (event.key === 'Enter') { sendData(input_text.value) } });
+
+function sendData(command: string) {
+    const requestOptions: RequestInit = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ "data": command })
+    };
+    output.innerHTML += command
+    input_text.value = ""
+    fetch('/terminal', requestOptions)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            let resp: string;
+            if (data.contect === "") { resp = `<br>${data.path}> ` }
+            else { resp = `<br>${data.contect} <br>${data.path}> ` }
+            output.innerHTML += resp
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        })
+}
+sendData("init")
